8bdeb0bef17c6dd23396b3b64867325ea560e518
Nam Phuong Do <dpnam2112@gmail.com>
2023-11-30 17:28:26 +0700

    Fix syntax error in backend/queries/triggers.sql

--- backend/queries/triggers.sql
+++ backend/queries/triggers.sql
@@ -1,4 +1,44 @@
 # Check if a user is old enough to be able to rate a film
+/*
+constraints to consider:
+ -	no two tv_series can have the same title 
+    and the same season number.
+    
+ - 	
+    
+triggers:
+ - 	trigger for updating film(rating_count)
+ - 	trigger for updating review(like_count)
+ - 	trigger for calculating episode(avg_score)
+*/
+
+CREATE TRIGGER upd_rating_count AFTER INSERT
+ON user_rates_films
+FOR EACH ROW
+	UPDATE film
+    SET rating_count = rating_count + 1
+    WHERE id = user_rates_films.film_id;
+    
+# Update like count of a review after a user likes a review
+CREATE TRIGGER upd_review_like_count AFTER INSERT
+ON user_like_review
+FOR EACH ROW
+	UPDATE review
+    SET like_count = like_count + 1
+    WHERE id = user_like_review.review_id;
+
+# Update episode's average score
+CREATE TRIGGER upd_episode_avg_score AFTER INSERT
+ON user_rates_episode
+FOR EACH ROW
+	UPDATE episode 
+    JOIN user_rates_episode ON episode.ep_number = user_rates_episode.ep_number
+    SET episode.avg_score = AVG(episode.score)
+    WHERE episode.ep_number = NEW.ep_number;
+
+delimiter //
+
+# Check if a user is old enough to be able to rate a film
 CREATE TRIGGER check_user_rates_films BEFORE INSERT
 ON user_rates_films
 FOR EACH ROW
@@ -7,7 +47,11 @@ BEGIN
 		SIGNAL SQLSTATE '45000'
 			SET message_text = "User is not eligible to rate the film.";
     END IF;
-END;
+END;//
+
+delimiter;
+
+delimiter //
 
 # Check if the start_year is not greater than end_year
 CREATE TRIGGER tv_series_compare_start_and_end BEFORE INSERT
@@ -18,4 +62,6 @@ BEGIN
         SIGNAL SQLSTATE '45000'
 			SET message_text = "end_year must not be less than start_year.";
     END IF;
-END;
+END;//
+
+delimiter ;
